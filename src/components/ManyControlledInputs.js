18a8132402595d61f11c86d41fc0d397aa2a5b95
chibokamc <chibokachinedum@gmail.com>
2022-11-19 00:05:24 +0100

    Multiple Inputs without multiple useStates per input

--- src/components/ManyControlledInputs.js
+++ src/components/ManyControlledInputs.js
@@ -2,29 +2,62 @@ import { useState } from "react";
 
 const ManyControlledInputs = () => {
 	const [person, setPerson] = useState({ firstName: "", email: "", age: "" });
+	const [people, setPeople] = useState([]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("hello");
+		// console.log("hello");
+		if (person.firstName && person.email && person.age) {
+			const newPerson = { ...person, id: new Date().getTime().toString() };
+			setPeople([...people, newPerson]);
+			setPerson({ firstName: "", email: "", age: "" });
+		}
+	};
+	// console.log(people, "people");
+
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setPerson({ ...person, [name]: value });
 	};
 
 	return (
 		<section>
 			<h2>Many Controlled Inputs</h2>
-			<form onSubmit={handleSubmit}>
+			<form>
 				<div>
-					<label htmlFor="name">Name : </label>
-					<input type="text" id="name" name="name" value={person.firstName} />
+					<label htmlFor="firstName">Name : </label>
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						value={person.firstName}
+						onChange={handleChange}
+					/>
 				</div>
 				<div>
 					<label htmlFor="email">Email : </label>
-					<input type="text" id="email" name="email" value={person.email} />
+					<input
+						type="text"
+						id="email"
+						name="email"
+						value={person.email}
+						onChange={handleChange}
+					/>
 				</div>
 				<div>
 					<label htmlFor="age">Age : </label>
-					<input type="text" id="age" name="age" value={person.age} />
+					<input
+						type="text"
+						id="age"
+						name="age"
+						value={person.age}
+						onChange={handleChange}
+					/>
 				</div>
-				<button type="submit">Submit Details</button>
+				<button type="submit" onClick={handleSubmit}>
+					Submit Details
+				</button>
 			</form>
 		</section>
 	);
